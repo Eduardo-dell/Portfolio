50b9807ae1ee32c367bf33084268f1be54956d86
Eduardo-dell <co.eduardo@outlook.com>
2020-11-19 16:24:21 -0300

    third commit

--- log.js
+++ log.js
@@ -3,4 +3,4 @@ const http = require("http")
 
 http.createServer((req,res) => {
   res.end("<h1>home </h1>")
-}).listem(5000, () => console.log("server is running.."))
+}).listen(5000, () => console.log("server is running.."))
